985a5c62f080fdef6515fedb1101a92487e4b3f2
chanyeong <jcy030896@naver.com>
2023-09-23 17:44:22 +0900

    config 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule } from './config/app/config.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AppConfigModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,54 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableCors({
+    // domain 확정되었을 때 origin 수정 필요
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
+  const appConfig = app.get(ConfigService);
+
+  app.enableShutdownHooks();
+
+  // const swaggerConfig = new DocumentBuilder()
+  //   .setTitle('SCG APPLY')
+  //   .setDescription('The API Description for SCG-APPLY-Backend')
+  //   .setVersion('1.0')
+  //   .addBearerAuth(
+  //     {
+  //       type: 'http',
+  //       scheme: 'bearer',
+  //       bearerFormat: 'JWT',
+  //       in: 'header',
+  //     },
+  //     'access-token',
+  //   )
+  //   .build();
+
+  // const document = SwaggerModule.createDocument(app, swaggerConfig);
+  // SwaggerModule.setup('api', app, document);
+  // console.log(`==== Running as ${process.env.APP_ENV} ====`);
+  await app.listen(appConfig.get('app.port'));
 }
 bootstrap();
